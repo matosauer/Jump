e36e0eea1042904f4a00bd5f332009a83b9a9b03
matosauer <ymatov@yahoo.com>
2016-03-13 08:00:40 +0200

    split platform position generating

--- js/GameLayer.js
+++ js/GameLayer.js
@@ -28,47 +28,102 @@ var GameLayer = cc.Layer.extend({
     
     initPlatforms: function(){
         
-        for(var i=0; i<1; i++){
-            this.addNextPlatform();
+        this.clearPlatofrms();
+        
+        var topPos = this.basePlatformPos();
+        this.addNextPlatform(topPos);
+        
+        for(var i=0; i<5; i++){
+            topPos = this.nextPlatformPos(topPos);
+            this.addNextPlatform(topPos);
         }
-
+        
     },
     
-    topPosPlatform: function(){
-        var top = 0;
+    basePlatformPos: function(){
+        
+        var size = cc.director.getWinSize();
+        
+        var pos = 
+            {
+                x: size.width / 2,
+                y: 100
+            };
+        
+        return pos;
+    },
     
-        for(var i=0; i < this.platforms.length; i++){
-            if (this.platforms[i].getPosition().y > top){
-                top = this.platforms[i].getPosition().y;
+    topPlatformPos: function(){
+        
+        var topPos = 
+            {
+                x: 0,
+                y: 0
+            };
+    
+        for(var i = 0; i < this.platforms.length; i++){
+            if (this.platforms[i].getPosition().y > topPos.y){
+                topPos = this.platforms[i].getPosition();
             }
         }
                                 
-        return top;
+        return topPos;
     },
     
-    addNextPlatform: function(){
+    nextPlatformPos: function(prevPos){
         
-        var yDistance = 100;
+        var size = cc.director.getWinSize();
         
-        var yPos = yDistance + this.topPosPlatform();
+        //var xPos = Math.floor(Math.random() * size.width) + min;
+        //var xPos = Math.floor(Math.random() * (size.width - g_settings.platform_1.w)) + g_settings.platform_1.w/2;
         
-		var xPos = 100;
+        var maxVerDistance = 10;  //depends on level?
+        var minVerOffset = 50; //depends on level?
+        var verMargin = g_settings.platform_1.h / 2;
+        var yPos = prevPos.y + minVerOffset + (maxVerDistance / 2 - Math.floor(Math.random() * maxVerDistance));
+        
+        var maxHorDistance = 100;  //depends on level?
+        var minHorOffset = 0;
+        var horMargin = g_settings.platform_1.w / 2;
+        var xPos = prevPos.x + minHorOffset + (maxHorDistance / 2 - Math.floor(Math.random() * maxHorDistance));
+        
+		if (xPos < horMargin) {
+            xPos = horMargin;
+        } else if (xPos > size.width - horMargin) {
+            xPos = size.width - horMargin;
+        }
+        
+        var pos = 
+            {
+                x: xPos,
+                y: yPos
+            };
+        
+        return pos;
+    },
+    
+    addNextPlatform: function(pos){
         
         var platform = new Platform(g_assets.IMG_PLATFORM);
-        platform.setPosition(xPos, yPos);
+        platform.setPosition(pos.x, pos.y);
         this.addChild(platform, g_settings.ZINDEX_PLATFORM);
         
         this.platforms.push(platform);
         
+        return platform;
+    },
+    
+    clearPlatofrms: function(){
         
-        var t = this.topPosPlatform();
-        
-        console.log(t);
+        while(this.platforms.length > 0) {
+            var platform = this.platforms.pop();
+            platform.removeFromParent();
+        }
         
     },
     
     removePlatform: function(platform){
-    
+        //A.splice(0,A.length)
     
     }
     
